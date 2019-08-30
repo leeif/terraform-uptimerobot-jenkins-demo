4653121c53844800f4f1289a76dddc655f4b2966
yifan-li <yifan.li@gree.net>
2019-08-30 11:23:25 +0900

    Update

--- monitors/http.tf
+++ monitors/http.tf
@@ -1,7 +1,19 @@
-resource "uptimerobot_monitor" "test_monitor" {
+resource "uptimerobot_monitor" "test_monitor_1" {
   friendly_name = "Test Monitor"
   type          = "http"
-  url           = "http://test.com"
+  url           = "http://test.com/1"
+  interval      = 300
+  alert_contact = [
+    {
+       id = "${var.alert_contact["Mail"]}"
+    }
+  ]
+}
+
+resource "uptimerobot_monitor" "test_monitor_2" {
+  friendly_name = "Test Monitor"
+  type          = "http"
+  url           = "http://test2.com/2"
   interval      = 300
   alert_contact = [
     {
