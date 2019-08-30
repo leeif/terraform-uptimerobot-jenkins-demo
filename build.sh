98b219f300a989757d642f2d4dc057c12c097089
yifan-li <yifan.li@gree.net>
2019-08-30 15:41:04 +0900

    remove real api_key

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
-API_KEY="u635167-59f3e8b1aa7a36d49e86448d"
+API_KEY="xxx"
 TERRAFORM_VERSION="0.11.14"
 docker build -t uptimerobot-terraform --build-arg TERRAFORM_VERSION=$TERRAFORM_VERSION .
 docker run -v ${PWD}:/uptimerobot -e API_KEY=$API_KEY uptimerobot-terraform 
